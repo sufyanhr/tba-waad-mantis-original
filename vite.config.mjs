63b694012c8d10ff00b80e10137560f84d50c98d
sufyanhr <sofy.fsh@gmail.com>
2025-12-07 18:32:07 +0200

    Initial commit

---
+++ vite.config.mjs
@@ -0,0 +1,37 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import jsconfigPaths from 'vite-jsconfig-paths';
+import path from 'path';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const API_URL = env.VITE_APP_BASE_NAME || '/';
+  const PORT = 3000;
+
+  return {
+    base: API_URL,
+    server: {
+      open: true,
+      port: PORT,
+      host: true
+    },
+    preview: {
+      open: true,
+      host: true
+    },
+    define: {
+      global: 'window' // Only if you need it for legacy packages
+    },
+    resolve: {
+      alias: {
+        '@ant-design/icons': path.resolve(__dirname, 'node_modules/@ant-design/icons')
+        // Add more aliases as needed
+      }
+    },
+    plugins: [react(), jsconfigPaths()],
+
+    optimizeDeps: {
+      include: ['@mui/material/Tooltip', 'react', 'react-dom', 'react-router-dom']
+    }
+  };
+});
